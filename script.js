c7c10933851452acec2d7a510bbd7f340a35b252
Ajay kumar <ajcoder25@gmail.com>
2024-01-11 06:51:49 +0530

    Learned about the data types

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
-const firstName = "Ajay";
-const last_name = "yadav";
+//string concatenation
+
+// const firstName = "Ajay";
+// const lastName = "yadav";
+
+// let fullName = firstName + + lastName;
 
 
-let fullName;
+// console.log('your full name is :' + fullName);
 
-console.log('Your fullName + '' + )
+
+
+//solving the concatenate strings #2
+
+//Numbers problem
+
+
+const score1 = 20;
+const score2 = 30;
+const score3 = 40;
+
+let totalScore = score1 + score2 + score3
+
+let averageScore = totalScore / 3;
+
+console.log(averageScore);
+console.log(totalScore);
+
+
+// Question-2
+
+const plates = 20;
+const people = 7;
+let reaminingPlate = plates - people;
+console.log(reaminingPlate)
+
+reaminingPlate += 1;
+
+
+
+
+//Data types 
+
+let numOne = 3
+
+let numTwo = 3
+
+console.log(numOne == numTwo);
 
