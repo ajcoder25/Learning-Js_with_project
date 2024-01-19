024f2eb722dc35bd764d7c85e3a9bc7f28fa9959
Ajay kumar <ajcoder25@gmail.com>
2024-01-19 15:45:17 +0530

    Trying to create Guess Number Game

---
+++ Number_Guessing_Game/script.js
@@ -0,0 +1,14 @@
+
+
+//Number GUessing Game 
+
+// Try to choose the minimum number and maximum num
+
+
+const minNum = 1;
+const maxNum = 100;
+
+
+const answer = Math.random() * (maxNum - minNum + 1);
+
+console.log(answer);
--- script.js
+++ script.js
@@ -308,12 +308,12 @@ if (time > 12) {
 
 /*
 
-ternary opertor = a shorcut to if{} and else {} statment helps to assign a variable based on a condition 
+ternary opertor = a shorcut to if{} and else {} statment helps to assign a variable based on a condition
                   condition ? codeIfTrue : codeIfFalse;
 
 
                 let age = 21
-                
+
                 let message = age>=18?"you're an adult": "you're a minor";  //If it is true then we assign the message (you're an adult ) and if it is false ('Youre a minor')
                 console.log(message)
 
@@ -326,17 +326,257 @@ ternary opertor = a shorcut to if{} and else {} statment helps to assign a varia
   console.log(greeting)
 
 
-  Example-2 
+  Example-2
 
    let isStudent = true;
 
    let Check = isStudent? "you are studend":"You have passed the exam";
 
 
-   Example -3 
+   Example -3
 
    let purchaseAmount = 125;
    let discount = purchaseAmount >=100 ? 10:0    //If its above 100 discount will be of 10% and if it is less than 100 it's 0
    console.log(`Your Total is $${purchaseAmount - PurchaseAmount * (discount/100)})
 
                 */
+
+
+
+///////////////////////////////////////////////////////////
+
+/* Switch = Can be an efficient Replacement to many else if statements
+
+let day = 1;
+switch(day){
+       case 1:
+         console.log("It is Monday");
+}
+
+
+Second Example -:
+
+let testScore = 90;
+let letterScore;
+
+switch(true){
+  case testScore >=90:
+    letterGrade = "A";
+    break;
+
+
+  case testScore >=80:
+    letterGrade = "B";
+    break;
+
+
+  case testScore >=50:
+    letterGrade = "C";
+    break;
+
+  case testScore >=60:
+    letterGrade = "D";
+    break;
+
+  case testScore >= 35:
+    letterGrade = "E";
+    break;
+
+  default:
+    letterGrade = "F";
+
+}
+
+
+console.log(letterGrade);
+
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+
+String Methods = allow you to manipulate  and work with the text(strings)
+
+
+let userName = "Bro Code";
+
+ // charAt() ---check the string word by its index
+
+console.log(userName.charAt(5));
+
+
+//indexof()---- show the index of that word you write in the console
+
+console.log(userName.indexOf("0"));
+
+
+//lastIndexOf
+
+console.log(userName.lastIndexOf("o"));
+
+//length properties
+
+console.log(userName.length);
+
+//trim()----it uses to trim any white space in the username 
+
+userName = userName.trim(); 
+console.log(userName);
+
+//toUpperCase();
+userName = userName.toUpperCase();
+
+//tolowerCase
+
+
+//startWith()--- to check the string if it is true or false
+
+let userName = "BroCode";
+let result = userName.startsWith("");
+
+console.log(result);//output---false
+
+
+//.endswith()--
+
+
+//replaceAll
+
+
+let phoneNumber = "123-456-7890";
+
+phoneNumber = phoneNumber.replaceAll("-","");///we did here replace the -- with empty string
+
+console.lgo(phoneNumber);
+
+
+//padStart()--
+
+
+
+//String Slicing = Creating a substring from a portion of another string 
+
+// String.slice(start,end)
+
+
+// const fullName = "Bro Code";
+
+// let firstName = fullName.slice(0,3);
+// console.log(firstName);
+
+// let lastName = fullName.slice(4,8);
+// console.log(lastName);
+
+
+// example -1 
+
+const email = "bro1@gmail.com";
+
+let userName = email.slice(0,email.indexOf("@"));
+
+console.log(userName);
+
+
+
+//Method chaining = Calling one method after another in one continuous line of code 
+
+//-- No Method chaining ---
+
+// we will tried to make a program where we try to enter the Email id where space will get trim, first latter become caps
+become capital and other than the first it become lowercase
+
+
+let userName = window.prompt("Enter your Username:");
+
+username = username.trim();
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
+
+let extrachars = username.slice(1);
+extrachars = extraChars.toLowerCase();
+username = letter + extraChars;
+
+console.log(username);
+
+
+
+
+//-- Method Chaining ---
+
+
+let username = window.prompt("Enter your username:");
+
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+
+console.log(username);
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Logical Opertor = used to combine or manipulate boolean values (true or false)
+
+AND = &&
+OR = ||
+Not = !
+
+const temp = 25;
+
+if (tem>20 && tem <100){
+  console.log('THe temperature is cool')
+}
+
+////////////////////////////////////////////////////////////////////////////
+
+// = assignment operator
+   == comparison Operator( compare if values are equal)
+   === Strict equality operator (compare if value & datatype are equal)
+   != inequality operator 
+   !== strictly inequality operator
+
+Example0
+
+const PI = "3.14";
+
+if (PI === "3.14"){
+  console.log("That is Pi");
+}else{
+  console.log("That is not Pi")
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//While Loop = Reapeat some code while some condition is true
+
+// for loop = repeat some code a limited amount of times
+
+
+
+for(let i = 0; i <=2; i++){
+  console.log(i);
+
+  
+}
+
+// Increment by 2 
+
+
+for (let i = 1; i<= 10; i+=2){
+  console.log(i);
+}
+
+// for loop = repeat some code a LIMITED amount of times
+
+// ------- INCREMENT -------
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+// ------- DECREMENT -------
+for(let i = 10; i > 0; i--){
+    console.log(i);
+}
+                                 
+
+
